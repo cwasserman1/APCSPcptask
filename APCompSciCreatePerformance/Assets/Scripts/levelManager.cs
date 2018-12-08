31de0127b40ff4f3e01e7ac0e7aeaf623062dd45
Cole Wasserman <coolcole0225@gmail.com>
2018-12-07 20:25:29 -0500

    committed again

--- APCompSciCreatePerformance/Assets/Scripts/levelManager.cs
+++ APCompSciCreatePerformance/Assets/Scripts/levelManager.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class levelManager : MonoBehaviour
 {
+
+    public GameObject Trump;
+    public GameObject Hillary;
    public void LoadScene(string sceneName){
         SceneManager.LoadScene(sceneName);
     }
     public void Quitapp(){
         Application.Quit();
     }
+    public void LoadTrump(){
+
+        Instantiate(Trump, new Vector3(0, 0, 0), Quaternion.identity);
+        Object.DontDestroyOnLoad(Trump);
+    }
+    public void LoadHillary(){
+      
+    }
+    
 }
